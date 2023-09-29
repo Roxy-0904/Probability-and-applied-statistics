f99324e3068e9f53566cc28f633de33095dc7449
hkren <hkren@134.210.231.173>
2023-09-29 09:14:13 -0400

    stats library

---
+++ method_libray/src/Stats_library_Ch2.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Stats_library_Ch2 
+{
+	public double findintersection(ArrayList<Double> userinputnum)
+	{
+	double sum = 0;
+	
+	//storing a value for clarity
+	double result = sum / userinputnum.size();
+	return result;
+	}
+	
+	public double findunion(ArrayList<Double> userinputnum)
+	{
+	double sum = 0;
+	
+	//storing a value for clarity
+	double result = sum / userinputnum.size();
+	return result;
+	}
+	
+	public double find_idependent_and_dependent_intersection(ArrayList<Double> userinputnum)
+	{
+	double sum = 0;
+	
+	//storing a value for clarity
+	double result = sum / userinputnum.size();
+	return result;
+	}
+	
+	public double find_exclusive_or_not_excclusive_union(ArrayList<Double> userinputnum)
+	{
+	double sum = 0;
+	
+	//storing a value for clarity
+	double result = sum / userinputnum.size();
+	return result;
+	}
+
+}
